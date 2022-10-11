2905083ac5c2b094b3262a32846a4e98f95a9293
manip_ego <52987267+manipEgo@users.noreply.github.com>
2022-10-11 08:00:36 +0800

    easy-table example

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'vue-easytable/libs/theme-dark/index.css'
+import VueEasytable from 'vue-easytable'
 
+Vue.use(VueEasytable)
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+  render: (h) => h(App),
+})
+
+/*new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app')*/
